f5d72e339e5c379b545f37c27675af68171f6c68
Aleksandr Khromov <hr82al@gmail.com>
2020-11-03 17:14:30 +0300

    Added a postion selector.

--- src/main/java/ru/haval/muTrainings/controllers/PositionController.java
+++ src/main/java/ru/haval/muTrainings/controllers/PositionController.java
@@ -2,13 +2,15 @@ package ru.haval.muTrainings.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.haval.muTrainings.accessingdatajpa.Position;
 import ru.haval.muTrainings.accessingdatajpa.PositionsRepository;
 
 @RestController
-public class TestController {
+public class PositionController {
 
     @Autowired
     PositionsRepository positionsRepository;
@@ -23,6 +25,12 @@ public class TestController {
             e.printStackTrace();
         }
         return json;
+    }
+
+    @RequestMapping("/set_position")
+    public String setPosition(@RequestBody String tmp) {
+        System.out.println(tmp);
+        return "ok";
     }
 }
 
--- src/main/resources/static/libs/mu_trainings.js
+++ src/main/resources/static/libs/mu_trainings.js
@@ -1,12 +1,9 @@
-var data;
+var gl;
 $(function(){
     /*$(".employee_position").select2({
         data: getPositions(),
     });*/
-    $(".employee_position").(function(e) {
-    }) select2({
-            data: getPositions(),
-        });
+    $(".employee_position").parent().click(positionSelect);
 /*    $(".employee_position").click(function(e){
         //Получаем id работника
         var userId = $(this).parent().attr("id").substr(2);
@@ -42,3 +39,34 @@ function getPositions() {
     return tmp;;
 }
 
+function positionSelect(e) {
+    console.log($(this).html()) ;
+        var cur = $(this).children()[0];
+        $.ajax({
+            url: "/get_positions",
+            success: function(res) {
+
+                $(cur).select2({
+                    data: $.parseJSON(res)
+                });
+                $(cur).on('select2:select', function (e) {
+                //Here select new a position of  thr employee.
+                    var data = e.params.data;
+                    $(this).text(e.params.text);
+                    var div = $(cur).parent();
+                     console.log(div.html("<div class=\"employee_position\">" + e.params.data.text + "</div>"));
+                     div.click(positionSelect);
+                });
+
+            }
+        });
+
+        $(this).off("click");
+
+        //FIXME
+       /* $.ajax({
+            url  : "/set_position",
+            type : "POST",
+            data : "{id: 3}"
+        });*/
+}
